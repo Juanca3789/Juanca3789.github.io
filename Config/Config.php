8ebe9c35a28244c0353c63289c025a00968b6b3c
Carlos <juancaclavijo2005@gmail.com>
2023-09-07 14:19:16 -0500

    Estableciendo Conexión Con Base Google Cloud

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
-    const server = "localhost"; //Servidor al que se desea conectar
+    const server = "104.197.243.7"; //Servidor al que se desea conectar
     const database = "chat"; //Base de datos del servidor
     const user = "root"; //Usuario con el que deseamos acceder
-    const password = ""; //Contraseña del usuario
+    const password = "SE#(h}ef>%KaDVn2"; //Contraseña del usuario
     const charset = "utf8"; //charset de la base de datos
 ?>
